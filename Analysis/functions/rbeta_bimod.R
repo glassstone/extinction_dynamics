df534c8b1b9914ebe7e1ce70ce4a9dba8e8286c1
Jimmy O'Donnell <pearljim3@hotmail.com>
2015-12-20 16:05:40 -0500

    fixed bad behavior with odd numbers

--- Analysis/functions/rbeta_bimod.R
+++ Analysis/functions/rbeta_bimod.R
@@ -7,9 +7,10 @@ rbeta_bimod <- function(
 	beta2shape1, 
 	beta2shape2
 	){
-	bins <- rbinom(n = n, size = 1, prob = 0.5)
-	betadraws1 <- rbeta(n = n/2, shape1 = beta1shape1, shape2 = beta1shape2)
-	betadraws2 <- rbeta(n = n/2, shape1 = beta2shape1, shape2 = beta2shape2)
+	# bins <- rbinom(n = n, size = 1, prob = 0.5)
+	nsplit <- c(round(n/2), n - round(n/2))
+	betadraws1 <- rbeta(n = nsplit[1], shape1 = beta1shape1, shape2 = beta1shape2)
+	betadraws2 <- rbeta(n = nsplit[2], shape1 = beta2shape1, shape2 = beta2shape2)
 	betadrawsboth <- c(betadraws1, betadraws2)
 	rbeta_bimod_out <- sample(x = betadrawsboth, size = n, replace = FALSE, prob = NULL)
 	return(rbeta_bimod_out)
